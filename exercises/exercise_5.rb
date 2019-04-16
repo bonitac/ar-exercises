a674b6bffccde9784e36dbc741fe49a4d48f859c
Bonita Choi <bonitac93@gmail.com>
2019-04-16 21:05:31 +0000

    added employees to stores 1 and 2

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+sum = Store.sum(:annual_revenue)
+puts "Total revenue of company: #{sum}"
+average = sum/Store.count
+puts "Average revenue of stores: #{average}"
+more_than_mil = Store.where("annual_revenue > ?", 1000000)
+
+
+puts more_than_mil.count

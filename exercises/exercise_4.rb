465afb870833a0a57c54278f96b39baeb14cf155
Bonita Choi <bonitac93@gmail.com>
2019-04-16 18:16:04 +0000

    finished exercise 4: fetching menswear stores and womenswear stores separately

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,22 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.new :name => "Surrey", :annual_revenue => 224000, :mens_apparel => false, :womens_apparel =>true
+surrey.save
+whistler = Store.new :name => "Whistler", :annual_revenue => 1900000, :mens_apparel => true, :womens_apparel => false
+whistler.save
+yaletown = Store.new :name => "Yaletown", :annual_revenue => 430000, :mens_apparel => true, :womens_apparel => true
+yaletown.save
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "Name: #{store.name} Annual Revenue: #{store.annual_revenue}"
+end
+
+@womens_less_than_mil = Store.where(womens_apparel: true, annual_revenue: (0)..(1000000))
+@womens_less_than_mil.each do |store|
+  puts "Name: #{store.name} Annual Revenue: #{store.annual_revenue}"
+end
+
+# Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.

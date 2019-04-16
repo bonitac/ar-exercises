92b084887d75f279bc6e435cd34e66d2a32a98b4
Bonita Choi <bonitac93@gmail.com>
2019-04-16 23:13:03 +0000

    custom validator function complete

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,25 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+@store2.employees.create(first_name: "", last_name: "blip", hourly_rate: 60).valid?
+@store2.employees.create(first_name: "bloop", last_name: "", hourly_rate: 60).valid?
+Employee.create(first_name: "bloop", last_name: "blip", hourly_rate: 60).valid?
+bloop = Store.new :name => "Bl", :annual_revenue => 190000, :mens_apparel => true, :womens_apparel => false
+bloop.save
+@store2.employees.create(first_name: "bloop", last_name: "bloooop", hourly_rate: 30).valid?
+@store2.employees.create(first_name: "bloop", last_name: "bloooop", hourly_rate: 320).valid?
+@store2.employees.create(first_name: "bloop", last_name: "bloooop", hourly_rate: 45.5).valid?
+blop = Store.new :name => "Bl", :annual_revenue => 0, :mens_apparel => true, :womens_apparel => false
+blop.save
+blooop = Store.new :name => "Blbkjn", :annual_revenue => 190000, :mens_apparel => false, :womens_apparel => false
+blooop.save
+
+store_name = gets.chomp
+@new_store = Store.new :name => store_name
+@new_store.save
+
+puts "Name error: #{@new_store.errors.details[:name]}"
+puts "Annual Revenue error: #{@new_store.errors.details[:annual_revenue]}"
+puts "Men's apparel error: #{@new_store.errors.details[:mens_apparel]}"
+puts "Women's apparel error: #{@new_store.errors[:womens_apparel]}"
+# puts @new_store.errors[:womens_apparel]
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,10 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0, only_integer: true}
+  validate :carry_clothes
+  def carry_clothes
+      errors.add(:mens_apparel, "need to carry either men's apparel or women's apparel") unless mens_apparel || womens_apparel #NO COLON
+      errors.add(:womens_apparel, "need to carry either men's apparel or women's apparel") unless mens_apparel || womens_apparel
+  end
 end
